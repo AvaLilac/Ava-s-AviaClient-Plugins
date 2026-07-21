452e9c0ae7fc01db18b8ebb46cc9ac021b5830af
AvaLilac <avalilac@noreply.codeberg.org>
2026-07-22 01:59:56 +0200

    has the ability to update

--- copytoclipboard.js
+++ copytoclipboard.js
@@ -1,3 +1,8 @@
+/*
+  @UPDATEURL: https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/copytoclipboard.js
+  @VERSION: 1.0
+*/
+
 (function () {
     if (window.__CLIPBOARD_COPY__) return;
     window.__CLIPBOARD_COPY__ = true;
