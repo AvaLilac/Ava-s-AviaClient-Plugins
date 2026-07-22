34b29df9b1e33f246667214a63ef46d1e7150280
AvaLilac <avalilac@noreply.codeberg.org>
2026-07-22 02:14:07 +0200

    Update Timestamps.js

--- Timestamps.js
+++ Timestamps.js
@@ -1,3 +1,8 @@
+/*
+  @UPDATEURL: https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/Timestamps.js
+  @VERSION: 1.0
+*/
+
 (function () {
 
 if (window.__AVIA_TIMESTAMPS__) return;
