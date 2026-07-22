f4d1ee45e7048cdb0e10ff37c955b1ed65ead34f
AvaLilac <avalilac@noreply.codeberg.org>
2026-07-22 02:07:13 +0200

    Update NoSmoothScroll.js

--- NoSmoothScroll.js
+++ NoSmoothScroll.js
@@ -1,3 +1,8 @@
+/*
+  @UPDATEURL: https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/NoSmoothScroll.js
+  @VERSION: 1.0
+*/
+
 (function () {
     if (window.__noSmoothScroll) return;
     window.__noSmoothScroll = true;
