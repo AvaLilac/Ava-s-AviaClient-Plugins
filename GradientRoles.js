9b95f25e904308bd0ea52d088c9b988440f05052
AvaLilac <avalilac@noreply.codeberg.org>
2026-07-22 02:04:10 +0200

    Update GradientRoles.js

--- GradientRoles.js
+++ GradientRoles.js
@@ -1,3 +1,8 @@
+/*
+  @UPDATEURL: https://codeberg.org/AvaLilac/Ava-s-AviaClient-Plugins/raw/branch/main/GradientRoles.js
+  @VERSION: 1.0
+*/
+
 (function () {
   if (window.__AVIA_GRADIENT_ROLE__) return;
   window.__AVIA_GRADIENT_ROLE__ = true;
